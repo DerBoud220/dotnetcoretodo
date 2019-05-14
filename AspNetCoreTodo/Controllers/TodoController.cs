0edd315d19db29d59029cb7d1df665771d99b493
Endeejay <derekboud@gmail.com>
2019-05-13 22:23:57 -0500

    init
    init

---
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreTodo.Controllers {
+    public class TodoController : Controller {
+        // Actions will go here
+        // Routes that are handled by controllers are called actions
+        // These are represented by methods in the controller class
+        public IActionResult Index() {
+            
+        }
+    }
+}
