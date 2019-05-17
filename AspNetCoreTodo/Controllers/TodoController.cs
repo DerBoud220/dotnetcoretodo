330228e89b2cc8a43408a458941c60f472511e02
Endeejay <derekboud@gmail.com>
2019-05-16 21:41:42 -0500

    Commiting actually working code

--- AspNetCoreTodo/Controllers/TodoController.cs
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace AspNetCoreTodo.Controllers {
-    public class TodoController : Controller {
-        // Actions will go here
-        // Routes that are handled by controllers are called actions
-        // These are represented by methods in the controller class
-        public IActionResult Index() {
+// namespace AspNetCoreTodo.Controllers {
+//     public class TodoController : Controller {
+//         // Actions will go here
+//         // Routes that are handled by controllers are called actions
+//         // These are represented by methods in the controller class
+//         public IActionResult Index() {
             
-        }
-    }
-}
+//         }
+//     }
+// }
